99e5883b6975f26d7acf8053788150e263784ab0
Daniel Stoyanov <danielstoyanov@abv.bg>
2021-08-15 18:24:40 +0300

    deleted introduction.txt and added Functional_Programming_Lab

---
+++ Java Advanced/Functional_Programming_Lab/_01SortEvenNumbers.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _01SortEvenNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> numbersAsString = Arrays.stream(scanner.nextLine().split(", "))
+                .collect(Collectors.toList());
+
+        String result = numbersAsString.stream()
+                .mapToInt(Integer::parseInt)
+                .filter(e -> e % 2 == 0)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(result);
+
+        String resultInAscendingOrder = Arrays.stream(result.split(", "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(resultInAscendingOrder);
+    }
+}
